d14120983c41e9820b1e7203f9ddce9498ef2233
Arielle <arielle_jaqueline@hotmail.com>
2020-01-22 13:21:38 -0500

    unity changes

---
+++ Ironwalls/Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+
+    float scrollSpeed = -6f;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 35);
+        transform.position = startPos + Vector2.up * newPos;
+    }
+}
