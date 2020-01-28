b64c84414b2282db7cc0e4face4650a01fcc0d5c
Jesse <jessegabriel1>
2020-01-27 19:33:13 -0500

    All files added to ironwalls folder

---
+++ Ironwalls/Assets/Scripts/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.velocity = transform.right * speed;
+    }
+}
