551736d039de9a215054dfab8efa4d48c45c3a54
Lyle <lylespurrell@hotmail.com>
2020-03-02 19:20:17 -0500

    Added Machine Gun Prefab (Rough Draft)

---
+++ Ironwalls/Assets/Scripts/Weapon Scripts/MachineGunBulletController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGunBulletController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    private Rigidbody2D rbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rbody.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        if (other.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Ironwalls/Assets/Scripts/Weapon Scripts/MachineGunController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGunController : MonoBehaviour
+{
+    [SerializeField] private int ammo = 30;
+    [SerializeField] private GameObject bullet;
+
+    private Transform bulletSpawn;
+    private float fireDelay = 0.0f;
+    private bool isFiring = false;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        bulletSpawn = gameObject.GetComponent<Transform>().Find("Bullet_Spawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isFiring = true;            
+        } 
+        else if (Input.GetMouseButtonUp(0)) 
+        {
+            isFiring = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isFiring == true)
+        {
+            time += Time.deltaTime;
+            if (time > 0.1f)
+            {
+                Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation = Quaternion.Euler(0, 0, Random.Range(-5.0f, 5.0f)));
+                time = 0;
+            }           
+        }
+    }
+}
