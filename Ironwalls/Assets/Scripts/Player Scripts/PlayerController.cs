d9689aee24714adc826e6169d3efe3f3981839ed
Lyle <lylespurrell@hotmail.com>
2020-03-09 17:14:19 -0400

    Fixed Dash Inversion Bug

--- Ironwalls/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Ironwalls/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -174,11 +174,11 @@ public class PlayerController : MonoBehaviour
         }
 
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
-        Vector3 mouse = Input.mousePosition;
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
-        Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
-        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        //Vector3 mouse = Input.mousePosition;
+        //Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        //Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        //float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0f, 0f, angle);
 
         //if (Input.GetMouseButtonDown(0))
         //{
---
+++ Ironwalls/Assets/Scripts/Player Scripts/PlayerController2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
