2ecaacbeb3992c46af0bff3e233811a4fffb9301
Lyle <lylespurrell@hotmail.com>
2020-03-09 15:58:05 -0400

    Changed Dash Button Combos

--- Ironwalls/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Ironwalls/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 8.0f;
-    [SerializeField] private float dash = 50.0f;
-    [SerializeField] private float dashDelayTime = 2.0f;
-    [SerializeField] private bool isCoolingDash = false;
     [SerializeField] private int weapon = 1;
     [SerializeField] private GameObject pistol;
     [SerializeField] private GameObject machineGun;
@@ -19,10 +15,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool isHolding = false;
     [SerializeField] private float throwCrate = 10.0f;
 
-    private int doubleTapW = 0;
-    private int doubleTapA = 0;
-    private int doubleTapS = 0;
-    private int doubleTapD = 0;
+    [SerializeField] private float moveSpeed = 8.0f;
+    [SerializeField] private float dash = 100.0f;
+    [SerializeField] private bool canDash = true;
+    [SerializeField] private bool isCoolingDash = false;
+    [SerializeField] private float dashDelayTime = 2.0f;
+    [SerializeField] private bool dashW = false;
+    [SerializeField] private bool dashA = false;
+    [SerializeField] private bool dashS = false;
+    [SerializeField] private bool dashD = false;
+    
     private float time = 0.0f;
     private GameObject cratePosition;
     private Rigidbody2D rb;
@@ -147,7 +149,7 @@ public class PlayerController : MonoBehaviour
             isHolding = false;
             numberOfCrates = 0;
             moveSpeed = 8;
-            isCoolingDash = false;
+            canDash = true;
             dashDelayTime = 2;
         }
 
@@ -167,7 +169,7 @@ public class PlayerController : MonoBehaviour
             numberOfCrates = 0;
             moveSpeed = 8;
             throwCrate = 10;
-            isCoolingDash = false;
+            canDash = true;
             dashDelayTime = 2;
         }
 
@@ -187,86 +189,82 @@ public class PlayerController : MonoBehaviour
         if (dashDelayTime != -1 && isCoolingDash == true)
         {
             time += Time.deltaTime;
-            
+
             if (time >= dashDelayTime)
             {
                 time = 0;
                 isCoolingDash = false;
-            } 
+            }
         }
 
         // W 
         if (isCoolingDash == false && Input.GetKeyDown(KeyCode.W))
         {
-            doubleTapW++;
+            dashW = true;
         }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
         {
-            doubleTapW = 0;
+            dashW = false;
         }
 
-        if (isCoolingDash == false && doubleTapW == 2)
+        if (canDash == true && isCoolingDash == false && dashW == true && Input.GetKeyDown(KeyCode.LeftShift))
         {
             rb.AddForce(transform.up * dash, ForceMode2D.Impulse);
-            doubleTapW = 0;
             isCoolingDash = true;
         }
 
         // A
         if (isCoolingDash == false && Input.GetKeyDown(KeyCode.A))
         {
-            doubleTapA++;
+            dashA = true;
         }
-        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
         {
-            doubleTapA = 0;
+            dashA = false;
         }
 
-        if (isCoolingDash == false && doubleTapA == 2)
+        if (canDash == true && isCoolingDash == false && dashA == true && Input.GetKeyDown(KeyCode.LeftShift))
         {
             rb.AddForce(-transform.right * dash, ForceMode2D.Impulse);
-            doubleTapA = 0;
             isCoolingDash = true;
         }
 
         // S
         if (isCoolingDash == false && Input.GetKeyDown(KeyCode.S))
         {
-            doubleTapS++;
+            dashS = true;
         }
-        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
         {
-            doubleTapS = 0;
+            dashS = false;
         }
 
-        if (isCoolingDash == false && doubleTapS == 2)
+        if (canDash == true && isCoolingDash == false && dashS == true && Input.GetKeyDown(KeyCode.LeftShift))
         {
             rb.AddForce(-transform.up * dash, ForceMode2D.Impulse);
-            doubleTapS = 0;
             isCoolingDash = true;
         }
 
         // D
         if (isCoolingDash == false && Input.GetKeyDown(KeyCode.D))
         {
-            doubleTapD++;
+            dashD = true;
         }
-        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S))
         {
-            doubleTapD = 0;
+            dashD = false;
         }
 
-        if (isCoolingDash == false && doubleTapD == 2)
+        if (canDash == true && isCoolingDash == false && dashD == true && Input.GetKeyDown(KeyCode.LeftShift))
         {
             rb.AddForce(transform.right * dash, ForceMode2D.Impulse);
-            doubleTapD = 0;
             isCoolingDash = true;
         }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        isCoolingDash = true;
+        canDash = false;
         if (isHolding == false && col.tag == "Crate")
         {
             numberOfCrates++;
@@ -280,7 +278,7 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerStay2D(Collider2D col)
     {
-        isCoolingDash = true;
+        canDash = false;
         if (Input.GetMouseButtonDown(1) && col.tag == "Crate")
         {
             if (numberOfDetectdCrates != 0)
@@ -293,7 +291,7 @@ public class PlayerController : MonoBehaviour
             dashDelayTime = -1;            
 
             isHolding = true;
-            isCoolingDash = true;            
+            canDash = false;            
             cratePosition.SetActive(true);
             HideWeapons();
             Destroy(col.gameObject);
@@ -304,7 +302,7 @@ public class PlayerController : MonoBehaviour
     {
         if (dashDelayTime != -1)
         {
-            isCoolingDash = false;
+            canDash = true;
         }
 
         if (isHolding == false && col.tag == "Crate")
