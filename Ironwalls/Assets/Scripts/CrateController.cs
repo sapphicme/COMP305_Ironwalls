0992ff1e0136ad3123e0bb610bfee2f29283b15c
Lyle <lylespurrell@hotmail.com>
2020-03-09 20:29:48 -0400

    Reduced speed of crate(s) when moved

---
+++ Ironwalls/Assets/Scripts/CrateController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float time;
+    [SerializeField] private bool isMoving;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (isMoving == true)
+        {
+            time += Time.deltaTime / 10;
+            rb.velocity /= time;
+            rb.angularVelocity /= time;
+        }
+
+        if (rb.velocity != Vector2.zero || rb.angularVelocity != 0)
+        {
+            isMoving = true;
+        }     
+        else {
+            isMoving = false;
+            time = 1.0f;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            time = 1.0f;
+            isMoving = false;
+        }
+    }
+}
